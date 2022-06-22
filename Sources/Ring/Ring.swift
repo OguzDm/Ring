4c630c1f342cf284d2ac752075dcc36688054ec8
Squiretoss <squiretoss17@gmail.com>
2022-06-22 13:23:00 +0300

    pause button added

--- Sources/Ring/Ring.swift
+++ Sources/Ring/Ring.swift
@@ -1,13 +1,6 @@
-//
-//  CircularProgressView.swift
-//  CircularTimer
-//
-//  Created by Oğuz Demirhan on 21.06.2022.
-//
-
 import SwiftUI
 
-enum CenterType {
+public enum CenterType {
     case percentage
     case remaining
     case percentageRemaining
@@ -17,9 +10,19 @@ enum CenterType {
 @available(macOS 10.15, *)
 @available(iOS 15, *)
 public struct RingProgressView: View {
+    
+    public init(totalValue: Int, mainColor: Binding<Color>, secondaryColor: Binding<Color>, centerType: CenterType, isButtonActive: Bool = false,completion: @escaping (Bool) -> () ) {
+        self.totalValue = totalValue
+        self._progressFrontColor = mainColor
+        self._progressBackColor = secondaryColor
+        self.centerType = centerType
+        self.isButtonActive = isButtonActive
+        self.completion = completion
+    }
     var totalValue: Int
     @Binding var progressFrontColor: Color
     @Binding var progressBackColor: Color
+    var isButtonActive: Bool
     var centerType: CenterType
     var completion: (Bool) -> ()
     public var body: some View {
@@ -28,7 +31,7 @@ public struct RingProgressView: View {
                 CircularView(totalValue: totalValue,
                              progressFrontColor: $progressFrontColor,
                              progressBackColor: $progressBackColor,centerType: centerType,
-                             changer: context.date) { completed in
+                             changer: context.date,isButtonActives: isButtonActive) { completed in
                     completion(completed)
                 }
             }
@@ -42,39 +45,70 @@ public struct RingProgressView: View {
 @available(macOS 10.15, *)
 public struct CircularView: View {
     @State var value = 0
+    @State var paused: Bool = false
     var totalValue: Int
     @Binding var progressFrontColor: Color
     @Binding var progressBackColor: Color
     var centerType: CenterType
     var changer: Date
+    var isButtonActives: Bool
     var completion: (Bool) -> ()
     
     public var body: some View {
         if #available(macOS 11.0, *) {
-            ZStack {
-                Circle()
-                    .stroke(lineWidth: 30)
-                    .fill(progressBackColor)
-                
-                Circle()
-                    .trim(from: 0, to: CGFloat(value ) / CGFloat(totalValue))
-                    .stroke(style: StrokeStyle(lineWidth: 30,lineCap: .round))
-                    .rotation(.degrees(-90))
-                    .fill(progressFrontColor)
-                
-                centerTextView()
-                
-            }
-            .onChange(of: changer) { _ in
-                if value == totalValue {
-                    completion(true)
-                }
-                else {
-                    //withAnimation {
-                    value += 1
+            VStack {
+                ZStack {
+                    Circle()
+                        .stroke(lineWidth: 30)
+                        .fill(progressBackColor)
+                    
+                    Circle()
+                        .trim(from: 0, to: CGFloat(value ) / CGFloat(totalValue))
+                        .stroke(style: StrokeStyle(lineWidth: 30,lineCap: .round))
+                        .rotation(.degrees(-90))
+                        .fill(progressFrontColor)
+                    
+                    centerTextView()
                     
                 }
+                .onChange(of: changer) { _ in
+                    if value == totalValue {
+                        completion(true)
+                    }
+                    else {
+                        //withAnimation {
+                        if !paused {
+                            value += 1
+                        }
+                        
+                    }
+                }
+                
+                if isButtonActives {
+                    HStack {
+                        
+                        Button {
+                            paused.toggle()
+                        }
+                        
+                        label: {
+                            ZStack{
+                                progressFrontColor
+                                    Image(systemName: paused ? "play.circle": "pause.circle")
+                                        .resizable()
+                                        .foregroundColor(.white)
+                                        .frame(width: 40, height: 40, alignment: .center)
+                            }
+                            .cornerRadius(40)
+                            .frame(width:80,height: 80)
+                        }
+                    }
+                    
+                    Spacer()
+                }
+               
             }
+           
         } else {
             // Fallback on earlier versions
         }
@@ -116,4 +150,5 @@ public struct CircularView: View {
         
     }
 }
+
 
